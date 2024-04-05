2e47a6d6cc5be6e9b19390b5bbfbe1be84321fcc
Ahmed Ragab <117094590+Ahmed41022@users.noreply.github.com>
2024-04-06 01:16:53 +0200

    first commit

--- launch_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo nvidia-docker build -t flownet2-pytorch:CUDA9-py35 .
-sudo nvidia-docker run --rm -ti --volume=$(pwd):/flownet2-pytorch:rw --workdir=/flownet2-pytorch --ipc=host flownet2-pytorch:CUDA9-py35 /bin/bash
--- networks/submodules.py
+++ networks/submodules.py
@@ -1,41 +1,63 @@
-# freda (todo) : 
+# freda (todo) :
 
 import torch.nn as nn
 import torch
-import numpy as np 
+import numpy as np
+import torch.nn.functional as F
+try:
+    from spatial_correlation_sampler import spatial_correlation_sample
+except ImportError as e:
+    import warnings
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings("default", category=ImportWarning)
+        warnings.warn(
+            "failed to load custom correlation module" "which is needed for FlowNetC",
+            ImportWarning,
+        )
+
 
 def conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1):
     if batchNorm:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=False),
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size-1)//2, bias=False),
             nn.BatchNorm2d(out_planes),
-            nn.LeakyReLU(0.1,inplace=True)
+            nn.LeakyReLU(0.1, inplace=True)
         )
     else:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
-            nn.LeakyReLU(0.1,inplace=True)
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size-1)//2, bias=True),
+            nn.LeakyReLU(0.1, inplace=True)
         )
 
-def i_conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1, bias = True):
+
+def i_conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1, bias=True):
     if batchNorm:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=bias),
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size-1)//2, bias=bias),
             nn.BatchNorm2d(out_planes),
         )
     else:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=bias),
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size-1)//2, bias=bias),
         )
 
+
 def predict_flow(in_planes):
-    return nn.Conv2d(in_planes,2,kernel_size=3,stride=1,padding=1,bias=True)
+    return nn.Conv2d(in_planes, 2, kernel_size=3, stride=1, padding=1, bias=True)
+
 
 def deconv(in_planes, out_planes):
     return nn.Sequential(
-        nn.ConvTranspose2d(in_planes, out_planes, kernel_size=4, stride=2, padding=1, bias=True),
-        nn.LeakyReLU(0.1,inplace=True)
+        nn.ConvTranspose2d(in_planes, out_planes, kernel_size=4,
+                           stride=2, padding=1, bias=True),
+        nn.LeakyReLU(0.1, inplace=True)
     )
+
 
 class tofp16(nn.Module):
     def __init__(self):
@@ -66,13 +88,93 @@ def init_deconv_bilinear(weight):
     weight.data.fill_(0.)
     for i in range(f_shape[0]):
         for j in range(f_shape[1]):
-            weight.data[i,j,:,:] = torch.from_numpy(bilinear)
+            weight.data[i, j, :, :] = torch.from_numpy(bilinear)
 
 
 def save_grad(grads, name):
     def hook(grad):
         grads[name] = grad
     return hook
+
+
+def conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1):
+    if batchNorm:
+        return nn.Sequential(
+            nn.Conv2d(
+                in_planes,
+                out_planes,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=(kernel_size - 1) // 2,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_planes),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(
+                in_planes,
+                out_planes,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=(kernel_size - 1) // 2,
+                bias=True,
+            ),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+
+
+def i_conv(batchNorm, in_planes, out_planes, kernel_size=3, stride=1, bias=True):
+    if batchNorm:
+        return nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size-1)//2, bias=bias),
+            nn.BatchNorm2d(out_planes),
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size-1)//2, bias=bias),
+        )
+
+
+def predict_flow(in_planes):
+    return nn.Conv2d(in_planes, 2, kernel_size=3, stride=1, padding=1, bias=False)
+
+
+def deconv(in_planes, out_planes):
+    return nn.Sequential(
+        nn.ConvTranspose2d(
+            in_planes, out_planes, kernel_size=4, stride=2, padding=1, bias=False
+        ),
+        nn.LeakyReLU(0.1, inplace=True),
+    )
+
+
+def correlate(input1, input2):
+    out_corr = spatial_correlation_sample(
+        input1,
+        input2,
+        kernel_size=1,
+        patch_size=21,
+        stride=1,
+        padding=0,
+        dilation_patch=2,
+    )
+    # collate dimensions 1 and 2 in order to be treated as a
+    # regular 4D tensor
+    b, ph, pw, h, w = out_corr.size()
+    out_corr = out_corr.view(b, ph * pw, h, w) / input1.size(1)
+    return F.leaky_relu_(out_corr, 0.1)
+
+
+def crop_like(input, target):
+    if input.size()[2:] == target.size()[2:]:
+        return input
+    else:
+        return input[:, :, : target.size(2), : target.size(3)]
+
 
 '''
 def save_grad(grads, name):
