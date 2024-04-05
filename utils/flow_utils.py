8eb37e1a9cfddf448390f5816d925d57eb930567
Ahmed Ragab <117094590+Ahmed41022@users.noreply.github.com>
2024-04-06 01:38:25 +0200

    modified

--- utils/flow_utils.py
+++ utils/flow_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 TAG_CHAR = np.array([202021.25], np.float32)
 
+
 def readFlow(fn):
     """ Read .flo file in Middlebury format"""
     # Code adapted from:
@@ -23,9 +24,10 @@ def readFlow(fn):
             # The reshape here is for visualization, the original code is (w,h,2)
             return np.resize(data, (int(h), int(w), 2))
 
-def writeFlow(filename,uv,v=None):
+
+def writeFlow(filename, uv, v=None):
     """ Write optical flow to file.
-    
+
     If v is None, uv is assumed to contain both u and v channels,
     stacked in depth.
     Original code by Deqing Sun, adapted from Daniel Scharstein.
@@ -33,23 +35,23 @@ def writeFlow(filename,uv,v=None):
     nBands = 2
 
     if v is None:
-        assert(uv.ndim == 3)
-        assert(uv.shape[2] == 2)
-        u = uv[:,:,0]
-        v = uv[:,:,1]
+        assert (uv.ndim == 3)
+        assert (uv.shape[2] == 2)
+        u = uv[:, :, 0]
+        v = uv[:, :, 1]
     else:
         u = uv
 
-    assert(u.shape == v.shape)
-    height,width = u.shape
-    f = open(filename,'wb')
+    assert (u.shape == v.shape)
+    height, width = u.shape
+    f = open(filename, 'wb')
     # write the header
     f.write(TAG_CHAR)
     np.array(width).astype(np.int32).tofile(f)
     np.array(height).astype(np.int32).tofile(f)
     # arrange into matrix form
     tmp = np.zeros((height, width*nBands))
-    tmp[:,np.arange(width)*2] = u
-    tmp[:,np.arange(width)*2 + 1] = v
+    tmp[:, np.arange(width)*2] = u
+    tmp[:, np.arange(width)*2 + 1] = v
     tmp.astype(np.float32).tofile(f)
     f.close()
