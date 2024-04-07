0abebb22492d1dfdbb4445ea52b01b5effeac591
Ahmed Ragab <117094590+Ahmed41022@users.noreply.github.com>
2024-04-07 21:48:12 +0200

    Custom layers

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from torch.nn import init
 
 import math
 import numpy as np
+import torch.nn.functional as F
 
 
 from networks import FlowNetC
@@ -26,19 +27,14 @@ class FlowNet2(nn.Module):
         self.div_flow = div_flow
         self.rgb_max = args.rgb_max
         self.args = args
-        # self.channelnorm = torch.linalg.norm(input, 2, dim=1, keepdim=True)
-
-        def channelnorm(self, input):
-            return torch.linalg.norm(input, 2, dim=1, keepdim=True)
-        # First Block (FlowNetC)
+        # self.channelnorm = channelnorm(input, 2, dim=1, keepdim=True)
+        # Block (FlowNetC)
         self.flownetc = FlowNetC.FlowNetC(args, batchNorm=self.batchNorm)
         self.upsample1 = nn.Upsample(scale_factor=4, mode='bilinear')
-        self.resample1 = nn.functional.interpolate
-
-        # Block (FlowNetS1)
+        # self.resample1 = nn.functional.interpolate
         self.flownets_1 = FlowNetS.FlowNetS(args, batchNorm=self.batchNorm)
         self.upsample2 = nn.Upsample(scale_factor=4, mode='bilinear')
-        self.resample2 = nn.functional.interpolate
+        # self.resample2 = nn.functional.interpolate
 
         # Block (FlowNetS2)
         self.flownets_2 = FlowNetS.FlowNetS(args, batchNorm=self.batchNorm)
@@ -48,8 +44,8 @@ class FlowNet2(nn.Module):
         self.upsample3 = nn.Upsample(scale_factor=4, mode='nearest')
         self.upsample4 = nn.Upsample(scale_factor=4, mode='nearest')
 
-        self.resample3 = nn.functional.interpolate
-        self.resample4 = nn.functional.interpolate
+        # self.resample3 = nn.functional.interpolate
+        # self.resample4 = nn.functional.interpolate
 
         # Block (FLowNetFusion)
         self.flownetfusion = FlowNetFusion.FlowNetFusion(args,
@@ -66,6 +62,9 @@ class FlowNet2(nn.Module):
                     init.uniform(m.bias)
                 init.xavier_uniform(m.weight)
                 # init_deconv_bilinear(m.weight)
+
+    def channelnorm(self, input):
+        return torch.linalg.norm(input, 2, dim=1, keepdim=True)
 
     def init_deconv_bilinear(self, weight):
         f_shape = weight.size()
@@ -90,7 +89,7 @@ class FlowNet2(nn.Module):
         rgb_mean = inputs.mean(dim=1, keepdim=True)
         print("Inputs shape:", inputs.shape)
         print("RGB mean shape:", rgb_mean.shape)
-        x = (inputs - rgb_mean) / 255.
+        x = (inputs - rgb_mean) / self.rgb_max
         print("x:", x.shape)
         # 255. = rgb_max
         x1 = x[:, :, 0, :, :]
@@ -107,8 +106,8 @@ class FlowNet2(nn.Module):
 
         # warp img1 to img0; magnitude of diff between img0 and and warped_img1,
         # resampled_img1 = self.resample1(x[:, 3:, :, :], flownetc_flow)
-        resampled_img1 = F.upsample_nearest(
-            x[:, 3:, :, :], output_size=flownetc_flow.size()[-2:])
+        resampled_img1 = F.interpolate(
+            x[:, 3:, :, :], size=flownetc_flow.size()[-2:], mode='nearest')
 
         diff_img0 = x[:, :3, :, :] - resampled_img1
         norm_diff_img0 = self.channelnorm(diff_img0)
@@ -122,7 +121,8 @@ class FlowNet2(nn.Module):
         flownets1_flow = self.upsample2(flownets1_flow2*self.div_flow)
 
         # warp img1 to img0 using flownets1; magnitude of diff between img0 and and warped_img1
-        resampled_img1 = self.resample2(x[:, 3:, :, :], flownets1_flow)
+        resampled_img1 = F.interpolate(
+            x[:, 3:, :, :], size=flownets1_flow.size()[-2:], mode='nearest')
         diff_img0 = x[:, :3, :, :] - resampled_img1
         norm_diff_img0 = self.channelnorm(diff_img0)
 
@@ -135,35 +135,25 @@ class FlowNet2(nn.Module):
         flownets2_flow = self.upsample4(flownets2_flow2 * self.div_flow)
         norm_flownets2_flow = self.channelnorm(flownets2_flow)
 
-        diff_flownets2_flow = self.resample4(x[:, 3:, :, :], flownets2_flow)
-        # if not diff_flownets2_flow.volatile:
-        #     diff_flownets2_flow.register_hook(save_grad(self.args.grads, 'diff_flownets2_flow'))
+        diff_flownets2_flow = F.interpolate(
+            x[:, 3:, :, :], size=flownets2_flow.size()[-2:], mode='nearest')
 
         diff_flownets2_img1 = self.channelnorm(
             (x[:, :3, :, :]-diff_flownets2_flow))
-        # if not diff_flownets2_img1.volatile:
-        #     diff_flownets2_img1.register_hook(save_grad(self.args.grads, 'diff_flownets2_img1'))
 
         # flownetsd
         flownetsd_flow2 = self.flownets_d(x)[0]
         flownetsd_flow = self.upsample3(flownetsd_flow2 / self.div_flow)
         norm_flownetsd_flow = self.channelnorm(flownetsd_flow)
 
-        diff_flownetsd_flow = self.resample3(x[:, 3:, :, :], flownetsd_flow)
-        # if not diff_flownetsd_flow.volatile:
-        #     diff_flownetsd_flow.register_hook(save_grad(self.args.grads, 'diff_flownetsd_flow'))
-
+        diff_flownetsd_flow = F.interpolate(
+            x[:, 3:, :, :], size=flownetsd_flow.size()[-2:], mode='nearest')
         diff_flownetsd_img1 = self.channelnorm(
             (x[:, :3, :, :]-diff_flownetsd_flow))
-        # if not diff_flownetsd_img1.volatile:
-        #     diff_flownetsd_img1.register_hook(save_grad(self.args.grads, 'diff_flownetsd_img1'))
 
         # concat img1 flownetsd, flownets2, norm_flownetsd, norm_flownets2, diff_flownetsd_img1, diff_flownets2_img1
         concat3 = torch.cat((x[:, :3, :, :], flownetsd_flow, flownets2_flow, norm_flownetsd_flow,
                             norm_flownets2_flow, diff_flownetsd_img1, diff_flownets2_img1), dim=1)
         flownetfusion_flow = self.flownetfusion(concat3)
-
-        # if not flownetfusion_flow.volatile:
-        #     flownetfusion_flow.register_hook(save_grad(self.args.grads, 'flownetfusion_flow'))
 
         return flownetfusion_flow
